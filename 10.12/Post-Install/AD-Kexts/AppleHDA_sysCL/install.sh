74145d0b62b919256103f4d4372972494752e0b6
Lockzi <lockzi@gmail.com>
2017-02-23 10:43:32 +0100

    HDMI Output fix for SMBIOS MacBookPro13,3
    Non-breaking fix for HDMI Output MacBookPro13,3

--- 10.12/Post-Install/AD-Kexts/AppleHDA_sysCL/install.sh
+++ 10.12/Post-Install/AD-Kexts/AppleHDA_sysCL/install.sh
@@ -12,6 +12,13 @@ sudo chmod 644 /Library/LaunchAgents/good.win.ALCPlugFix.plist
 sudo chown root:wheel /Library/LaunchAgents/good.win.ALCPlugFix.plist
 sudo launchctl load /Library/LaunchAgents/good.win.ALCPlugFix.plist
 sudo rm -Rf /Library/Extensions/aDummyHDA.kext
+
+#Fix for HDMI output MBP13,3 Lockzi
+if [[ $(var_ID=$(ioreg -p IODeviceTree -r -n / -d 1 | grep board-id);var_ID=${var_ID##*<\"};var_ID=${var_ID%%\">};echo $var_ID) = *Mac-A5C67F76ED83108C* ]]; then
+  echo "Applying MacBookPro 13,3 SMBIOS HDMI Output Fix"
+  sudo sed -i.vanilla 's/Mac-FC02E91DDD3FA6A4/Mac-A5C67F76ED83108C/g' /System/Library/Extensions/AppleGraphicsControl.kext/Contents/PlugIns/AppleGraphicsDevicePolicy.kext/Contents/Info.plist
+fi
+
 sudo rm -rf /System/Library/Caches/com.apple.kext.caches/Startup/kernelcache  
 sudo rm -rf /System/Library/PrelinkedKernels/prelinkedkernel  
 sudo touch /System/Library/Extensions && sudo kextcache -u /
